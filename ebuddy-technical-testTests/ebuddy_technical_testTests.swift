12911e3212d6e91295dca78747e4776dbb669193
andiasrafil <41411347+andiasrafil@users.noreply.github.com>
2024-12-13 13:04:24 +0700

    test

--- ebuddy-technical-testTests/ebuddy_technical_testTests.swift
+++ ebuddy-technical-testTests/ebuddy_technical_testTests.swift
@@ -27,5 +27,10 @@ struct ebuddy_technical_testTests {
         #expect(true)
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
+    
+    @Test func example5() async throws {
+        #expect(true)
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
 
 }
