989a094b568bd1cf4e919f781830efc13e29cee5
andiasrafil <41411347+andiasrafil@users.noreply.github.com>
2024-12-13 10:04:15 +0700

    test

--- ebuddy-technical-testTests/ebuddy_technical_testTests.swift
+++ ebuddy-technical-testTests/ebuddy_technical_testTests.swift
@@ -10,6 +10,11 @@ import Testing
 struct ebuddy_technical_testTests {
 
     @Test func example() async throws {
+        
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+    @Test func example2() async throws {
+        
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 
